25660387163714853aa7a3a977d9a1750cd231c2
Mumtaz Fatima <88557841+mumtazf@users.noreply.github.com>
2022-12-08 23:09:11 -0500

    Updated with audio speech code
    the function is called in line 68. It is declared in line 71

--- vizable_front/App.js
+++ vizable_front/App.js
@@ -3,12 +3,20 @@ import { Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import * as Permissions from "expo-permissions";
 import React, { useEffect, useReducer, useRef, useState } from "react";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Audio } from 'expo-av';
+
+
 
 import {
-  StatusBar,
   Text,
-  Button,
   View,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+  Platform,
+  NativeModules,
+  Dimensions, 
 } from "react-native";
 
 //setting our initial app state
@@ -18,23 +26,57 @@ const cameraState = {
   zoomValue: 0,
 };
 
+const winHeight = Dimensions.get('window').height;
+const winWidth = Dimensions.get('window').width;
+
+
+
+
+
 export default function App() {
+
+  //for adding the sound
+  const [sound, setSound] = React.useState();
+
+
+  const deviceLanguage =
+      Platform.OS === 'ios'
+        ? NativeModules.SettingsManager.settings.AppleLocale ||
+          NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13 // make diff selection
+        : NativeModules.I18nManager.localeIdentifier;
+        
   state = cameraState;
 
 //checking device permissions for camera
   const [hasPermission, setHasPermission] = useState(null);
   const { cameraType } = state;
 
-  const camera =  useState();
+  const camera =  useRef();
+
+  const [showCaption, setScaption] = useState(false);
+  
 
 //function for clicking picture
   const clickPicture = async () => {    
       let photo = await camera.current.takePictureAsync();
       const image_location = photo.uri; //store the cache location of the image
+      //maybe change 
+      setScaption(true)
       camera.current.pausePreview();
       await savePicture(image_location); //save it to the camera roll
       camera.current.resumePreview();
+      playSound(); //here's where the sound is played
   };
+
+  async function playSound() {
+    console.log('Recording is being played');
+    const { sound } = await Audio.Sound.createAsync( require('./recording/caption_recording.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
 
 //saving the image to camera roll
   const savePicture = async (photo: string) => {
@@ -55,7 +97,15 @@ export default function App() {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
     })();
-  }, []);
+
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
 
   if (hasPermission === null) {
     return <View />;
@@ -64,17 +114,53 @@ export default function App() {
     return <Text>Please provide access to your camera</Text>;
   }
 
-
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar />
+    <View style={{ flex: 1, backgroundColor:  "#e6f3ff"}}>
+      <StatusBar/>
+      <View style = {{flexDirection: "row"}}>
+        <Text style ={{flex:1, fontFamily: "monospace", fontSize: 25, fontWeight: "bold", color: "#8187FD", textAlign: "center", marginLeft: "4%", marginVertical: "1%"}}> Vizable</Text>
+        <Ionicons name = 'information-circle-sharp' style ={{fontSize: 34,alignSelf: "center",marginRight: "2%", color: '#8187FD' }}></Ionicons>
+      </View>
+      
+      {/* {<!--Caption component -->} */}
       <Camera
         ref={camera}
-        style={{ flex: 1 }}
+        style={{ flex:1}} //change back 
         type={cameraType}
-      />
-      <Button title ="Take Picture" size={100} onPress={clickPicture} />
+      />     
+      { showCaption &&
+        <View style = {{height: 100,width: winWidth*.95,  backgroundColor: 'white', borderRadius: 8, alignSelf: "center",position: "absolute", top: 60, paddingHorizontal: "2%"}}> 
+        <Text style = {{fontSize: 20, adjustFontSizeToFit: true, allowFontScaling: true, fontWeight: "bold", color: "black", opacity: 1}} >
+          Dim desk with chocolate and flask and books
+        </Text>
+        </View>
+      }
+      <TouchableOpacity  style = {styles.btnSub} onPress = {clickPicture} >
+        <Text style = {{fontSize: 20, adjustFontSizeToFit: true, allowFontScaling: true, fontWeight: "bold", color: "white", opacity: 1}}>Take Picture</Text>
+      </TouchableOpacity>
+      
+      
       </View>
+      
+
 
   );
+  
 }
+//responsive text
+const styles = StyleSheet.create({
+  btnSub: {
+    width: winWidth*.95,
+    height: winHeight*.13,
+    alignSelf: 'center',
+    maxHeight: 300,
+    backgroundColor: "rgba(29, 40, 251, .7)",
+    alignItems: "center",
+    justifyContent: "center", 
+    marginHorizontal: "2%", //need to center it 
+    position: "absolute",
+    bottom: winHeight*.02,
+    borderRadius: 8,
+
+  }
+})
